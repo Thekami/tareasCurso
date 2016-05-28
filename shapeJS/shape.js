1a04a6be7815c6da718c8f61f768e2352bdf562e
Angel Torres <thekamitorres@gmail.com>
2016-05-28 14:00:05 -0500

    casi listo

--- shapeJS/shape.js
+++ shapeJS/shape.js
@@ -19,6 +19,9 @@ var DivObj;
 var ancho;
 var alto;
 var borde;
+var color;
+var rgb;
+var arr;
 
 function AgregarElemento(){
 	DivObj=document.createElement("div");
@@ -26,14 +29,21 @@ function AgregarElemento(){
 	ancho = document.getElementById('IdAncho')
 	alto = document.getElementById('IdAlto')
 	borde = document.getElementById('IdBorde')
+	color = document.getElementById('IdColorBack')
 	DivObj.style.width = "100px"
 	DivObj.style.height = "100px"
+	DivObj.style.borderWidth = "1px"
+	DivObj.style.backgroundColor = "#FF0000"
+
 	DivObj.setAttribute("onclick","removeBorder(); "+
 								  "DivObj = this; "+
 								  "DivObj.style.border = 'dashed 3px';"+
 								  "ancho.value = DivObj.style.width.split('px')[0];"+
 								  "alto.value = DivObj.style.height.split('px')[0];"+
-								  "borde.value = DivObj.style.borderWidth.split('px')[0];");
+								  "borde.value = DivObj.style.borderWidth.split('px')[0];"+
+								  "rgb = DivObj.style.backgroundColor.split('(')[1].split(')')[0].split(',');"+
+								  "color.value = rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]));"+
+								  "console.log(DivObj.style.backgroundColor)");
 	document.getElementById("SecContainer").appendChild(DivObj);
 }
 
@@ -42,3 +52,23 @@ function removeBorder(){
 	for(var i = 0; i < clases.length; i++)
 		clases[i].style.border = ''
 }
+
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+rgb = 'rgb(255, 51, 0)'.split('(')[1].split(')')[0].split(',')
+// rgb = rgb.split(')')[0]
+// rgb = rgb.split(',')
+
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+// 	rgbToHex(0, 51, 255)
+// console.log(rgbToHex(0, 51, 255))
+// console.log(rgb)
+// console.log(rgbToHex(parseInt(rgb[2]), parseInt(rgb[1]), parseInt(rgb[0])))
